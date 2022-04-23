1b50eb20fba99a5fe14aacfb479c0869e087f51b
george.papadakis <george.papadakis@oddschecker.com>
2022-04-23 23:09:15 +0300

    Backend initial commit

---
+++ backend_sanity/schemas/workExperience.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'workExperience',
+  title: 'Work Experience',
+  type: 'document',
+  fields: [
+    {
+      name: 'jobTitle',
+      title: 'Job Title',
+      type: 'string'
+    },
+    {
+      name: 'company',
+      title: 'Company',
+      type: 'string'
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    }
+  ]
+}
